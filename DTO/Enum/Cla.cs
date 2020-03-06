822103a4520335491652f7cabc064156a8908a16
Ilson de Arruda Rodrigues <cejacpd@yahoo.com.br>
2020-03-06 14:25:43 -0300

    adição de equipe e jounnins

--- DTO/Enum/Cla.cs
+++ DTO/Enum/Cla.cs
@@ -11,6 +11,7 @@ namespace DTO.Enum
         Amagiri,
         Hagoromo,
         Haruno,
+        Hatake,
         Hyuga,
         Inuzuka,
         Kaguya,
---
+++ DTO/EquipeDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DTO
+{
+    public class EquipeDTO
+    {
+        public int ID { get; set; }
+        public List<GenninDTO> Gennin { get; set; }
+        public List<JounninDTO> Jounnin { get; set; }
+
+
+        public EquipeDTO()
+        {
+            Gennin = new List<GenninDTO>();
+            Jounnin = new List<JounninDTO>();
+        }
+    }
+}
