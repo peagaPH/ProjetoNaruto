1ca84f7b4f3395ddc932eecdf9e04aee0c633d9a
Tomás SBJ <tomassbj@gmail.com>
2020-03-06 14:52:52 -0300

    Atualização DAO

--- DAO/ChuninContext.cs
+++ DAO/ChuninContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using DTO;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,10 +8,15 @@ namespace DAO
 {
     public class ChuninContext : DbContext
     {
-        public ChuninContext() : base(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\900180\Documents\SSNeco.mdf;Integrated Security=True;Connect Timeout=30")
+       public ChuninContext() : base(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\hbsis\Documents\SSNeco.mdf;Integrated Security=True;Connect Timeout=30")
         {
 
         }
+
+        public DbSet<GenninDTO> Gennins { get; set; }
+        public DbSet<EquipeDTO> Equipes { get; set; }
+        public DbSet<JounninDTO> Jounnins { get; set; }
+
 
     }
 }
