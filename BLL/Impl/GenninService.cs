513c1960a013c9e91aa79b302c29388fc921b14b
Tomás SBJ <tomassbj@gmail.com>
2020-03-06 17:08:56 -0300

    Atualização do Services

--- BLL/Impl/GenninService.cs
+++ BLL/Impl/GenninService.cs
@@ -1,22 +1,75 @@
 ﻿using BLL.Impl;
 using BLL.Interfaces;
+using Common;
+using DAO;
 using DTO;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace BLL
 {
     public class GenninService : BaseService, IGenninService
     {
-        public Task<List<GenninDTO>> GetData()
+        public async Task<List<GenninDTO>> GetData()
         {
-            throw new NotImplementedException();
+            try
+            {
+                using (ChuninContext context = new ChuninContext())
+                {
+                    return await context.Gennins.ToListAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                File.WriteAllText("log.txt", ex.Message + " - " + ex.StackTrace);
+                throw new Exception("Erro no banco de dados, contate o administrador");
+            }
         }
 
-        public Task Insert(GenninDTO gennin)
+        public async Task Insert(GenninDTO gennin)
         {
-            throw new NotImplementedException();
+            List<Error> errors = new List<Error>();
+
+            if (string.IsNullOrWhiteSpace(gennin.Nome))
+            {
+                base.AddError("Nome", "Nome do ninja deve ser informado.");
+            }
+            else if (gennin.Nome.Length < 5 || gennin.Nome.Length > 50)
+            {
+                base.AddError("Nome", "O nome deve conter entre 3 e 50 caracteres.");
+            }
+
+            if (string.IsNullOrWhiteSpace(gennin.Idade))
+            {
+                base.AddError("Idade", "Idade do ninja deve ser informada.");
+            }
+            else if (gennin.Idade.Length < 7)
+            {
+                base.AddError("Idade", "O ninja deve conter pelo menos 7 anos.");
+            }
+            base.CheckErrors();
+
+            //APÓS VALIDAR TODOS OS CAMPOS, VERIFIQUE SE POSSUIMOS ERROS
+
+            try
+            {
+                using (ChuninContext context = new ChuninContext())
+                {
+                    context.Gennins.Add(gennin);
+                    await context.SaveChangesAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                File.WriteAllText("log.txt", ex.Message + " - " + ex.StackTrace);
+                throw new Exception("Erro no banco de dados, contate o admnistrador.");
+            }
+
+
+
         }
     }
 }
