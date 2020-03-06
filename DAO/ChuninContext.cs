6e7f12ba36bb9232e40098fa707cb3fde84ef1bf
Tomás SBJ <tomassbj@gmail.com>
2020-03-06 14:44:15 -0300

    ChuninContext

---
+++ DAO/ChuninContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAO
+{
+    public class ChuninContext : DbContext
+    {
+        public ChuninContext() : base(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\900180\Documents\SSNeco.mdf;Integrated Security=True;Connect Timeout=30")
+        {
+
+        }
+
+    }
+}
