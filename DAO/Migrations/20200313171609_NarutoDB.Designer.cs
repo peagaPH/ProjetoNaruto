0e0237affa7b7330cd92fc39279eecbc2d5428ec
Tomás SBJ <tomassbj@gmail.com>
2020-03-13 14:21:38 -0300

    gfdghjklç

--- DAO/Migrations/20200313171609_NarutoDB.Designer.cs
+++ DAO/Migrations/20200313171609_NarutoDB.Designer.cs
@@ -3,14 +3,16 @@ using DAO;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DAO.Migrations
 {
     [DbContext(typeof(ChuninContext))]
-    partial class ChuninContextModelSnapshot : ModelSnapshot
+    [Migration("20200313171609_NarutoDB")]
+    partial class NarutoDB
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
