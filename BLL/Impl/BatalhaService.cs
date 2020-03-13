460a1ba3e75391efcaf25a7b67efeb1474757217
Ilson de Arruda Rodrigues <cejacpd@yahoo.com.br>
2020-03-13 16:28:11 -0300

    Service Batalha

---
+++ BLL/Impl/BatalhaService.cs
@@ -0,0 +1,26 @@
+﻿using BLL.Interfaces;
+using DAO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLL.Impl
+{
+    public class BatalhaService : BaseService, IBatalhaService
+    {
+        private ChuninContext _context;
+        public BatalhaService(ChuninContext ctx)
+        {
+            this._context = ctx;
+        }
+        public async Task<List<BatalhaDTO>> GetBatalhas()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Insert(BatalhaDTO batalha)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BLL/Interfaces/IBatalhaService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLL.Interfaces
+{
+    public interface IBatalhaService
+    {
+        Task Insert(BatalhaDTO batalha);
+        Task<List<BatalhaDTO>> GetBatalhas();
+
+
+    }
+}
